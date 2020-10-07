87dca421548260c5b48648f7e95ee60cc728a90d
JakesProjects1 <jacob@kagon.net>
2020-10-07 14:57:26 -0400

    new changes made

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
 # your code goes here
+require 'pry'
+class Person 
+
+def initialize(name)
+   @name = name
+   @bank_account = 25
+end
+
+def name
+  @name
+end
+
+def bank_account
+  @bank_account
+end
+
+end
+stella = Person.new("Stella")
+stella.name
+
+blake = Person.new("Blake") 
+blake.bank_account
+
+
